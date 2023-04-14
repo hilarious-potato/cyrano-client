06a6eacc2895c703d25da78bd6961a84b69bce7b
denefi <denefi@posteo.de>
2023-04-14 12:21:52 +0200

    even more setup and add potato

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App"> Cyrano!
-   
-    </div>
-  )
+  return <div className="App text-9xl">Cyrano!</div>;
 }
 
-export default App
+export default App;
