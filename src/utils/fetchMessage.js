0b63487086eb9c50300a534dfd10d9dd33f39bd8
denefi <denefi@posteo.de>
2023-04-17 21:41:06 +0200

    add decrypt message util

--- src/utils/fetchMessage.js
+++ src/utils/fetchMessage.js
@@ -1,5 +1,21 @@
-function fetchMessage() {
-  return (  );
+import axios from "axios";
+import decryptMessage from "./decryptMessage";
+
+async function fetchMessage(id, password) {
+  try {
+    console.log("We are now in fetchMessage Method");
+    const messageFromDb = await axios.get(
+      import.meta.env.VITE_APP_SERVER_URL + "/api/messages/" + id
+    );
+    console.log(messageFromDb);
+    const decryptedMessage = await decryptMessage(
+      messageFromDb.data.encryptedContent,
+      password
+    );
+    return decryptedMessage;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export default fetchMessage;
