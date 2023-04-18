8b39afbafe5c2bf5420291187965807677ceb2b1
denefi <denefi@posteo.de>
2023-04-18 14:49:14 +0200

    add console log for netflify variables

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./views/Home";
 import ReadMessage from "./views/ReadMessage";
+console.log(import.meta.env.DEPLOY_PRIME_URL);
+console.log(import.meta.env.URL);
+
 function App() {
   return (
     <div className="App h-screen w-screen bg-gray-800 text-gray-200">
