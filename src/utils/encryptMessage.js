1af0fb2117fa4e25d253b4472b680d7cbe61e141
denefi <denefi@posteo.de>
2023-04-19 11:29:19 +0200

    add new create key function

--- src/utils/encryptMessage.js
+++ src/utils/encryptMessage.js
@@ -1,48 +1,34 @@
 import { arrayBufferToB64, b64ToArrayBuffer } from "./base64encoder";
+import createKey from "./createKey";
 
 async function encryptMessage(message, password) {
-  const salt = window.crypto.getRandomValues(new Uint8Array(16));
-  // console.log("salt", salt);
-  const iv = window.crypto.getRandomValues(new Uint8Array(12));
-  // console.log("iv:", iv);
-  const encoder = new TextEncoder();
-  const encoded_message = encoder.encode(message);
-  const keyMaterial = await window.crypto.subtle.importKey(
-    "raw",
-    encoder.encode(password),
-    { name: "PBKDF2" },
-    false,
-    ["deriveBits", "deriveKey"]
-  );
-  const key = await window.crypto.subtle.deriveKey(
-    {
-      name: "PBKDF2",
-      salt: salt,
-      iterations: 10000,
-      hash: "SHA-256",
-    },
-    keyMaterial,
-    {
-      name: "AES-GCM",
-      length: 256,
-    },
-    true,
-    ["encrypt", "decrypt"]
-  );
-  const ciphertext = await window.crypto.subtle.encrypt(
-    { name: "AES-GCM", iv: iv },
-    key,
-    encoded_message
-  );
-  const encryptedMessage = await arrayBufferToB64(ciphertext);
-  const encodedSalt = await arrayBufferToB64(salt);
-  const encodedIV = await arrayBufferToB64(iv);
+  try {
+    console.log("encrypting message");
+    const encoder = new TextEncoder();
+    const encoded_message = encoder.encode(message);
+    const salt = window.crypto.getRandomValues(new Uint8Array(16));
+    const iv = window.crypto.getRandomValues(new Uint8Array(12));
 
-  return {
-    encryptedMessage,
-    iv: encodedIV,
-    salt: encodedSalt,
-  };
+    const key = await createKey(password, salt);
+
+    const ciphertext = await window.crypto.subtle.encrypt(
+      { name: "AES-GCM", iv: iv },
+      key,
+      encoded_message
+    );
+
+    const encryptedMessage = await arrayBufferToB64(ciphertext);
+    const encodedSalt = await arrayBufferToB64(salt);
+    const encodedIV = await arrayBufferToB64(iv);
+
+    return {
+      encryptedMessage,
+      iv: encodedIV,
+      salt: encodedSalt,
+    };
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export default encryptMessage;
