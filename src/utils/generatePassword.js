82f92eb1cd26efcc290a7ddaed10eef6b8761608
denefi <denefi@posteo.de>
2023-04-19 11:29:32 +0200

    add new generate Password function

--- src/utils/generatePassword.js
+++ src/utils/generatePassword.js
@@ -1,7 +1,6 @@
 function generatePassword() {
   const array = new Uint8Array(20);
   window.crypto.getRandomValues(array);
-  console.log(array);
 
   const password = Array.from(array)
     .map((i) => i.toString(16).padStart(2, "0"))
