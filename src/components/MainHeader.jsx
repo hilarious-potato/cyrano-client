a4a96c6e4cf0414fad09bdce6fa7d266d2b9c666
denefi <denefi@posteo.de>
2023-04-20 20:32:02 +0200

    add full responsiveness to header

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -18,14 +18,17 @@ const Header = (props) => {
         <h1 className="font-body text-4xl italic text-primary ">cyrano</h1>
       </Link>
 
-      <Link className="order-3 col-span-3 col-start-4 grid" to="/write">
+      <Link
+        className="order-3 col-span-3 col-start-4 grid md:col-span-2 md:col-start-5 xl:col-span-1 xl:col-start-6"
+        to="/write"
+      >
         <Button className="" primary>
           write a message
         </Button>
       </Link>
 
       {!isLoggedIn && (
-        <div className="col-span-4 grid grid-cols-2 gap-2">
+        <div className="col-span-4 grid grid-cols-2 gap-2 md:col-span-3 md:col-start-4 xl:col-span-2 xl:col-start-5 xl:gap-4">
           <Button
             className=""
             onClick={() => {
@@ -45,13 +48,16 @@ const Header = (props) => {
       )}
       {isLoggedIn && (
         <>
-          <Link className="order-2 col-span-3 grid" to="/tresors">
+          <Link
+            className="order-2 col-span-3 grid md:col-span-2 md:col-start-3 xl:col-span-1 xl:col-start-5 "
+            to="/tresors"
+          >
             <Button className="" secondary>
               your tresors
             </Button>
           </Link>
           <Button
-            className="order-1 col-span-3 col-start-4 "
+            className="order-1 col-span-3 col-start-4 md:col-span-2 md:col-start-5 xl:col-span-1 xl:col-start-6 "
             onClick={() => {
               handleLogout();
             }}
