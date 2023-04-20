193b44e523e833066f2a8c9b390c24f77c78a680
denefi <denefi@posteo.de>
2023-04-20 15:58:08 +0200

    add card view to tresor views

--- src/views/Tresors.jsx
+++ src/views/Tresors.jsx
@@ -2,6 +2,9 @@ import { useContext, useEffect, useState } from "react";
 import tresorService from "../services/tresor.service";
 import CreateTresor from "../components/CreateTresor";
 import { AuthContext } from "../context/auth.context";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import { Link } from "react-router-dom";
 
 function Tresors() {
   const { authenticateUser, user } = useContext(AuthContext);
@@ -18,19 +21,43 @@ function Tresors() {
     fetchTresors();
     // setTresorList(user.tresors);
   }, [user]);
+  const handleDelete = (tresorId) => {
+    tresorService.deleteTresor(tresorId).then((deletedTresor) => {
+      console.log("we deleted", deletedTresor);
+      fetchTresors();
+    });
+  };
   return (
-    <>
-      <h2>Create Tresor</h2>
+    <section className="xxl:w-1/3 relative lg:w-2/3">
       <CreateTresor fetchTresors={fetchTresors} />
-      {tresorList &&
-        tresorList.length > 0 &&
-        tresorList.map((tresor) => (
-          <div key={tresor._id}>
-            <h2>{tresor.title}</h2>
-            <a href={"/tresors/" + tresor._id}>Open Tresor</a>
-          </div>
-        ))}
-    </>
+      <Header title="your tresors" />
+      <div>
+        {tresorList &&
+          tresorList.length > 0 &&
+          tresorList.map((tresor) => (
+            <div
+              className="my-4 grid w-full gap-2 rounded-xl p-4 outline-1 hover:outline  hover:outline-gray-500"
+              key={tresor._id}
+            >
+              <h2 className="text-xl ">{tresor.title}</h2>
+              <div className="grid w-full grid-cols-4 gap-2">
+                <Button
+                  onClick={() => {
+                    handleDelete(tresor._id);
+                  }}
+                  warning
+                  className="col-start-3"
+                >
+                  delete
+                </Button>
+                <Button primary className="">
+                  <Link to={"/tresors/" + tresor._id}>open</Link>
+                </Button>
+              </div>
+            </div>
+          ))}
+      </div>
+    </section>
   );
 }
 
