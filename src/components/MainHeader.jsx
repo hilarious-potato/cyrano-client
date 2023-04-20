5904ee4c16a919f6bd5f7d5adf50dcab748b95fa
denefi <denefi@posteo.de>
2023-04-20 18:55:59 +0200

    header for small screen

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -13,54 +13,53 @@ const Header = (props) => {
   };
 
   return (
-    <header className="sticky top-0 z-10 flex items-baseline justify-between bg-gray-800/25 px-4 py-4 backdrop-blur-sm md:px-8 xl:px-16">
-      <Link to="/">
-        <h1 className="font-body text-4xl italic text-primary">cyrano</h1>
+    <header className="sticky top-0 z-10 grid grid-cols-6 items-baseline gap-4 bg-gray-800/25 px-4 py-4 backdrop-blur-sm md:px-8 xl:px-16">
+      <Link className="col-span-2" to="/">
+        <h1 className="font-body text-4xl italic text-primary ">cyrano</h1>
       </Link>
 
-      <div>
-        <Link to="/write">
-          <Button className="mr-4" primary>
-            write a message
-          </Button>
-        </Link>
+      <Link className="order-3 col-span-3 col-start-4 grid" to="/write">
+        <Button className="" primary>
+          write a message
+        </Button>
+      </Link>
 
-        {!isLoggedIn && (
-          <>
-            <Button
-              className="mr-2"
-              onClick={() => {
-                props.toggleSignup();
-              }}
-            >
-              register
+      {!isLoggedIn && (
+        <div className="col-span-4 grid grid-cols-2 gap-2">
+          <Button
+            className=""
+            onClick={() => {
+              props.toggleSignup();
+            }}
+          >
+            register
+          </Button>
+          <Button
+            onClick={() => {
+              props.toggleLogin();
+            }}
+          >
+            login
+          </Button>
+        </div>
+      )}
+      {isLoggedIn && (
+        <>
+          <Link className="order-2 col-span-3 grid" to="/tresors">
+            <Button className="" secondary>
+              your tresors
             </Button>
-            <Button
-              onClick={() => {
-                props.toggleLogin();
-              }}
-            >
-              login
-            </Button>
-          </>
-        )}
-        {isLoggedIn && (
-          <>
-            <Link to="/tresors">
-              <Button className="mr-4" secondary>
-                your tresors
-              </Button>
-            </Link>
-            <Button
-              onClick={() => {
-                handleLogout();
-              }}
-            >
-              logout
-            </Button>
-          </>
-        )}
-      </div>
+          </Link>
+          <Button
+            className="order-1 col-span-3 col-start-4 "
+            onClick={() => {
+              handleLogout();
+            }}
+          >
+            logout
+          </Button>
+        </>
+      )}
     </header>
   );
 };
